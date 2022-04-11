5cc8c97e863de50ad37c6a770d4ca650eb6f296c
Joris Nonnast <joris.nonnast@stud.h-da.de>
2022-04-11 16:20:00 +0200

    use pybluez for bluetooth recv

--- Python/Hardware/bluetooth/recv.py
+++ Python/Hardware/bluetooth/recv.py
@@ -1,11 +1,10 @@
-import socket
+import bluetooth
 
 if __name__ == "__main__":
     address = "7C:7A:91:75:D8:2E"
     port = 3
-    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s = bluetooth.BluetoothSocket()
     s.bind((address, port))
-    s.listen(1)
     client, address = s.accept()
     data = client.recv(1024)
     print(data)
