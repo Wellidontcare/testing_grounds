479573936aecef187d01e93a90accb625c2c493a
Joris Nonnast <joris.nonnast@stud.h-da.de>
2022-04-17 12:26:10 +0200

    add fresnel notebook

--- Python/Hardware/bluetooth/send.py
+++ Python/Hardware/bluetooth/send.py
@@ -1,8 +1,15 @@
 import socket
-
-address = "7C:7A:91:75:D8:2E"
+import time
+address = "84:a6:c8:10:ca:79"
 port = 3
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.connect((address, port))
-text = input()
-s.send(bytes(text, 'UTF-8'))
+counter = 0
+while True:
+    text = str(time.time())
+    start = time.perf_counter()
+    s.send(bytes(text, 'UTF-8'))
+    end = time.perf_counter()
+    print(f"Sending Data with {1 / (end - start)} FPS!")
+    counter += 1
+    print(time.time())
